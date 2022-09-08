84e8c695124f61ccb2b16a3dfb85042f02fd6e28
Teiyem <teethedev@gmail.com>
2022-09-08 13:19:11 -0700

    push, pall

---
+++ README.md
@@ -0,0 +1,15 @@
+# Monty
+# 0x19. C - Stacks, Queues - LIFO, FIFO
+Diving deeper into C stacks, queues - LIFO, FIFO
+
+## Learning Objectives:
+* What do LIFO and FIFO mean
+* What is a stack, and when to use it
+* What is a queue, and when to use it
+* What are the common implementations of stacks and queues
+* What are the most common use cases of stacks and queues
+* What is the proper way to use global variables
+
+## Project Configuration
+* VIM 8.1.3741 
+* Ubuntu 20.04 LTS
---
+++ monty.h
@@ -0,0 +1,58 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern int retval;
+void print_node_pall(stack_t **h, unsigned int line_number);
+void nop_node(stack_t **h, unsigned int line_number);
+void pop_node(stack_t **head, unsigned int line_number);
+void swap_first_second(stack_t **h, unsigned int line_number);
+void push_node(stack_t **head, int n);
+void print_first_pint(stack_t **h, unsigned int line_number);
+void add_first_second(stack_t **h, unsigned int line_number);
+size_t list_len(stack_t *h);
+int find_opcode(char **token, unsigned int line_number, stack_t **head);
+void free_array(char **array);
+void free_list(stack_t *head);
+char **tokenize(char *line, unsigned int line_number);
+int is_empty(char *str);
+int is_int(char *str);
+
+#endif /* MONTY_H */
